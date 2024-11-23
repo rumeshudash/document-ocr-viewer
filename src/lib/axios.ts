57603c62303aabbfb1f05146b122ae37e687522e
Rumesh Udash <rumesh.udas@gmail.com>
2024-11-23 23:12:59 +0545

    updated axios base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL:
         process.env.NEXT_PUBLIC_VERCEL_URL ??
-        process.env['__NEXT_PRIVATE_ORIGIN'],
+        process.env['__NEXT_PRIVATE_ORIGIN'] ??
+        'http://localhost:3000',
     headers: {
         'Content-Type': 'application/json',
     },
